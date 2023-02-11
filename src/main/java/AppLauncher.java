0d1918d749cb8644a2c3272f023a3b074dbe8ea3
VicZhukov <vityok593@gmail.com>
2023-02-11 10:29:25 +0200

    Create repository

---
+++ src/main/java/AppLauncher.java
@@ -0,0 +1,23 @@
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import serviceClasses.CurrencyDataBase;
+import settings.Settings;
+
+public class AppLauncher {
+    public static void main(String[] args) throws TelegramApiException {
+        CurrencyDataBase currencyDataBase = new CurrencyDataBase();
+        Settings settings = new Settings(currencyDataBase);
+        settings.load();
+
+
+        Timer timer = new Timer(settings);
+        Thread messageInTime = new Thread(timer);
+        messageInTime.start();
+
+        CurrencyInfoBot currencyInfoBot = CurrencyInfoBot.getInstance("currencyInfoBot", settings);
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(currencyInfoBot);
+    }
+}
