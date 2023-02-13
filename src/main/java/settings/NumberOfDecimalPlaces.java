0c30aadd362afb9f402386db0334b5815beebcf3
Roman Cherkhavskyi <romko-chera@ukr.net>
2023-02-13 16:02:55 +0200

    In setting package add classes and enum's:
    - BotInfo - in class save data about bot_name and bot_token
    
    - Buttons - in enum saved names for buttons
    
    - NumberOfDecimalPlaces - enum include methods for change how many digits we can see before koma
    
    - ZoneId - class include all UTC time zones time correctives

---
+++ src/main/java/settings/NumberOfDecimalPlaces.java
@@ -0,0 +1,51 @@
+package settings;
+
+public enum NumberOfDecimalPlaces {
+    TWO(2, "twoPlaces", false),
+    THREE(3, "threePlaces", false),
+    FOUR(4, "fourPlaces", false);
+
+    private String nameDecPlaces;
+    private int intNumber;
+    private boolean select;
+
+    NumberOfDecimalPlaces(int intNumber, String nameDecPlaces, boolean select) {
+        this.intNumber = intNumber;
+        this.nameDecPlaces = nameDecPlaces;
+        this.select = select;
+    }
+
+    public int getIntNumber() {
+        return intNumber;
+    }
+
+    public void setIntNumber(int intNumber) {
+        this.intNumber = intNumber;
+    }
+
+    public String getNameDecPlaces() {
+        return nameDecPlaces;
+    }
+
+    public void setNameDecPlaces(String nameDecPlaces) {
+        this.nameDecPlaces = nameDecPlaces;
+    }
+
+    public boolean isSelect() {
+        return select;
+    }
+
+    public void setSelect(boolean select) {
+        this.select = select;
+    }
+
+    public static NumberOfDecimalPlaces convertToEnum(String text) {
+        for (NumberOfDecimalPlaces decimalPlaces : NumberOfDecimalPlaces.values()) {
+            if (decimalPlaces.getNameDecPlaces().equals(text)) {
+                return decimalPlaces;
+            }
+        }
+        return null;
+    }
+}
+
